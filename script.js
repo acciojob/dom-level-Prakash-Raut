f9df5fee1ac1afcf1587f3c8d8c37ab9c3b673fe
Prakash Raut <prakash_raut@outlook.com>
2025-07-30 15:42:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+
+function getDomLevel() {
+	const targetElement = document.getElementById("level");
+	
+	if (!targetElement) {
+        alert("Element not found");
+        return;
+    }
+	
+	let level = 0;
+	let current = targetElement;
+	
+	while (current) {
+        level++;
+        current = current.parentElement;
+    }
+
+    alert(`The level of the element is: ${level}`);
+
+}
+
+getDomLevel();
